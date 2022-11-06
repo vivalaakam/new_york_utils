673de0d592f78c11089d972902d9ca7944e584c3
vivalaakam <viva.la.akam@gmail.com>
2022-11-06 07:39:10 +0300

    v0.1.8

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 pub use file::{exists_file, read_from_file, write_to_file};
+pub use hash_md5::hash_md5;
 pub use levenshtein::levenshtein;
 pub use make_id::make_id;
 pub use matrix::Matrix;
+pub use range::range;
+pub use to_nearest::{ceil_to_nearest, floor_to_nearest, round_to_nearest};
 
 mod error;
 mod file;
+mod hash_md5;
 mod levenshtein;
 mod make_id;
 mod matrix;
+mod range;
+mod to_nearest;
---
+++ src/to_nearest.rs
@@ -0,0 +1,11 @@
+pub fn round_to_nearest(num: f64, base: f64) -> f64 {
+    (num / base).round() * base
+}
+
+pub fn floor_to_nearest(num: f64, base: f64) -> f64 {
+    (num / base).floor() * base
+}
+
+pub fn ceil_to_nearest(num: f64, base: f64) -> f64 {
+    (num / base).ceil() * base
+}
