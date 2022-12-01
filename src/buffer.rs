f0c248d2e16891dfe2c106048b0e54ff669aab07
Andrey Makarov <viva.la.akam@gmail.com>
2022-12-01 12:43:46 +0400

    v0.1.10

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,25 +1,30 @@
-pub struct Buffer {
-    vals: Vec<f64>,
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
+pub struct Buffer<T> {
+    vals: Vec<T>,
     pub(crate) len: usize,
     pushes: usize,
     pub(crate) index: usize,
-    pub(crate) sum: f64,
+    pub(crate) sum: T,
 }
 
-impl Buffer {
+impl<T> Buffer<T>
+where
+    T: Default + Clone + Copy + Add + Sub + SubAssign + AddAssign,
+{
     pub fn new(len: usize) -> Self {
         Buffer {
-            vals: vec![0.0; len],
+            vals: vec![T::default(); len],
             len,
             pushes: 0,
             index: 0,
-            sum: 0.0,
+            sum: T::default(),
         }
     }
 
-    pub fn push(&mut self, val: f64) {
+    pub fn push(&mut self, val: T) {
         if self.pushes >= self.len {
-            self.sum -= self.vals[self.index];
+            self.sum -= self.vals.get(self.index).expect("").clone();
         }
 
         self.sum += val;
@@ -31,7 +36,7 @@ impl Buffer {
         }
     }
 
-    pub fn qpush(&mut self, val: f64) {
+    pub fn qpush(&mut self, val: T) {
         self.vals[self.index] = val;
         self.index += 1;
         if self.index >= self.len {
@@ -39,7 +44,37 @@ impl Buffer {
         }
     }
 
-    pub fn get(&self, ind: usize) -> f64 {
-        self.vals[(self.index + self.len - 1 + ind) % self.len]
+    pub fn get(&self, ind: usize) -> T {
+        self.vals
+            .get((self.index + self.len - 1 + ind) % self.len)
+            .expect("")
+            .clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn buffer_f64() {
+        let mut buffer = Buffer::new(2);
+        buffer.push(1.);
+        assert_eq!(1.0, buffer.sum);
+        buffer.push(1.5);
+        assert_eq!(2.5, buffer.sum);
+        buffer.push(2.5);
+        assert_eq!(4.0, buffer.sum);
+    }
+
+    #[test]
+    fn buffer_u64() {
+        let mut buffer: Buffer<u64> = Buffer::new(2);
+        buffer.push(1);
+        assert_eq!(1, buffer.sum);
+        buffer.push(2);
+        assert_eq!(3, buffer.sum);
+        buffer.push(3);
+        assert_eq!(5, buffer.sum);
     }
 }
