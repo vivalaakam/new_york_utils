732e6baafe6106e7117a5f054c9dddb12c2d3196
Andrey Makarov <viva.la.akam@gmail.com>
2022-12-01 12:01:24 +0400

    v0.1.9

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub use buffer::Buffer;
 pub use file::{exists_file, read_from_file, write_to_file};
 pub use hash_md5::hash_md5;
 pub use levenshtein::levenshtein;
@@ -6,6 +7,7 @@ pub use matrix::Matrix;
 pub use range::range;
 pub use to_nearest::{ceil_to_nearest, floor_to_nearest, round_to_nearest};
 
+mod buffer;
 mod error;
 mod file;
 mod hash_md5;
